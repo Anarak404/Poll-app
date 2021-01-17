cfbcbb62c76002609f6ca19d88a7de347990ad1a
Anarak404 <annaarak2@gmail.com>
2021-01-17 23:44:35 +0100

    Add database

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'SuperSecretKey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/poll'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question_text = db.Column(db.String(250))
+    answers = db.relationship('Answer', backref='question')
+
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    answer_text = db.Column(db.String(150))
+    votes = db.Column(db.Integer)
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
 
 
 @app.route('/', methods=['GET'])
